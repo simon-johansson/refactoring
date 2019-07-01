969688d4ab52bb74d93a17cd6ec9a49cdcba8052
Simon Johansson <mail@simon-johansson.com>
2019-07-01 22:18:10 +0200

    Updates

---
+++ precache-manifest.cebbec7aa5003132755ebfb238d6c61a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce3aaa20653d6bd685f6281ac9a70f8",
+    "url": "/refactoring/index.html"
+  },
+  {
+    "revision": "cffd3576f686fe470972",
+    "url": "/refactoring/static/css/main.31bec733.chunk.css"
+  },
+  {
+    "revision": "1020f65bf9ffebe313f0",
+    "url": "/refactoring/static/js/2.49c41374.chunk.js"
+  },
+  {
+    "revision": "cffd3576f686fe470972",
+    "url": "/refactoring/static/js/main.62771815.chunk.js"
+  },
+  {
+    "revision": "6233c1dcd25ea65e4a24",
+    "url": "/refactoring/static/js/runtime~main.956d6bd7.js"
+  }
+]);
